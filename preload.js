b9a5fe813fd27f308162fcadbb7ca1efa8e0951d
Diego Pontes <diego9us@gmail.com>
2024-11-01 20:28:56 -0300

    window estoque

--- preload.js
+++ preload.js
@@ -3,4 +3,7 @@ const { contextBridge, ipcRenderer } = require('electron')
 contextBridge.exposeInMainWorld('api', {
     fecharJanela: () => ipcRenderer.send('close-about'),
     janelaClientes: () => ipcRenderer.send('open-client')
+    janelaFornecedores: () => ipcRenderer.send('open-supplier')
+    janelaProdutos: () => ipcRenderer.send('open-product')
+    janelaRelatorios: () => ipcRenderer.send('open-report')
 })
