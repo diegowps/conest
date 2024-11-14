f6ea12f39dd6219ad213f44dbd19313313204a3a
Diego Umbelino <diego9us@gmail.com>
2024-11-13 22:21:30 -0300

    config db connected

--- preload.js
+++ preload.js
@@ -1,6 +1,11 @@
 const { contextBridge, ipcRenderer } = require('electron')
 
+//estabelecer a conexao com o banco de dados(envio pedido para a main)
+
+ipcRenderer.send('dbConnected')
+
 contextBridge.exposeInMainWorld('api', {
+    dbMessage:(message) => ipcRenderer.on('db-message', message),
     fecharJanela: () => ipcRenderer.send('close-about'),
     janelaClientes: () => ipcRenderer.send('open-client'),
     janelaFornecedores: () => ipcRenderer.send('open-supplier'),
