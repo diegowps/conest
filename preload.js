e0f6990b5fd2da3a70e80e18e93258f6e043f8c7
Diego Pontes <diego9us@gmail.com>
2024-12-02 22:25:02 -0300

    review

--- preload.js
+++ preload.js
@@ -1,15 +1,16 @@
 const { contextBridge, ipcRenderer } = require('electron')
 
-//estabelecer a conexao com o banco de dados(envio pedido para a main)
-
-ipcRenderer.send('dbConnected')
+// Estabelecer a conexão com o banco (envio de pedido para o main abrir a conexão com o banco de dados)
+ipcRenderer.send('db-connect')
 
 contextBridge.exposeInMainWorld('api', {
-    dbMessage:(message) => ipcRenderer.on('db-message', message),
+    dbMensagem: (message) => ipcRenderer.on('db-message', message),
     fecharJanela: () => ipcRenderer.send('close-about'),
     janelaClientes: () => ipcRenderer.send('open-client'),
     janelaFornecedores: () => ipcRenderer.send('open-supplier'),
     janelaProdutos: () => ipcRenderer.send('open-product'),
-    janelaEstoque: () => ipcRenderer.send('open-stock'),
-    janelaRelatorios: () => ipcRenderer.send('open-report')
+    janelaRelatorios: () => ipcRenderer.send('open-report'),
+    novoCliente: (cliente) => ipcRenderer.send('new-client', cliente),
+    novoFornecedor: (fornecedor) => ipcRenderer.send('new-fornecedor', fornecedor),
+    resetarFormulario: (args) => ipcRenderer.on('reset-form', args)    
 })
